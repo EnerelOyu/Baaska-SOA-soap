675bd547436897ffd60b41567896ee9b8aaf0b61
Baaska <baasandorj040402@gmail.com>
2026-05-01 10:36:37 +0800

    first commit

--- src/main/java/com/example/soap/SoapApplication.java
+++ src/main/java/com/example/soap/SoapApplication.java
@@ -2,8 +2,12 @@ package com.example.soap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example")
+@EnableJpaRepositories(basePackages = "com.example.model")
+@EntityScan(basePackages = "com.example.model")
 public class SoapApplication {
 
     public static void main(String[] args) {
